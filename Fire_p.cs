307d7278d15071fe5f26a2ef0a1f6ee975f9d4a9
IlusionX <32546663+IlusionX@users.noreply.github.com>
2022-04-11 19:35:46 -0300

    Add files via upload
    Main code of partcile(for real this time :v)

---
+++ Fire_p.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire_p : MonoBehaviour
+{
+    Color c;
+    SpriteRenderer sr;
+    int state = 0;
+    float contador = 0;
+    float flutuarSpeed;
+
+    void Start()
+    {
+        sr = this.gameObject.GetComponent<SpriteRenderer>();
+        c = sr.color;
+        flutuarSpeed = Random.RandomRange(1, 3);
+    }
+    
+    void Update()
+    {
+        switch (state) {
+            case 0:
+                Ignite();
+                break;
+            case 1:
+                CoolUp();
+                break;
+        }
+        this.gameObject.GetComponent<SpriteRenderer>().color = c;
+        contador += Time.deltaTime;
+    }
+
+    void Ignite() {
+        this.transform.localScale += Vector3.one * Time.deltaTime;
+        c.g += 0.4f * Time.deltaTime;
+        if (contador > 1) {state = 1;contador = 0; }
+
+        transform.position += Vector3.up * Time.deltaTime * flutuarSpeed;
+    }
+
+    void CoolUp() {
+        this.transform.localScale -= Vector3.one * Time.deltaTime;
+        c.r -= 0.5f * Time.deltaTime;
+        c.g -= 0.5f * Time.deltaTime;
+        c.b -= 0.5f * Time.deltaTime;
+        if (contador > 2) { Destroy(this.gameObject); }
+
+        transform.position += Vector3.up * Time.deltaTime * flutuarSpeed/3;
+    }
+}
